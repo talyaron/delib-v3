a3a8c68cf2088bb83e8369441f26d2978ad5e3ea
Tal Yaron <tal.yaron@gmail.com>
2019-01-27 08:43:22 +0200

    deconsole log

--- src/functions/firebase/get/get.js
+++ src/functions/firebase/get/get.js
@@ -6,21 +6,19 @@ function getUserGroups(userId) {
     DB.collection('users').doc(userId).collection('groupsOwned').onSnapshot(groupsOwnedDB => {
         const groupsNumber = groupsOwnedDB.size;
         let count = 0;
-        console.log(groupsNumber)
+       
         let groupArray = [];
         groupsOwnedDB.forEach(groupOwnedDB => {
             //listen to the groups and update...
             DB.collection('groups').doc(groupOwnedDB.data().id).onSnapshot(groupDB => {
-                // console.log(groupDB.data());
-                groupArray.push(groupDB.data());
-                console.dir(store.userGroups)
+              
+                groupArray.push(groupDB.data());               
                 count++;
-                console.log(count)
+                
                 if (count == groupsNumber) {
                     store.userGroups = groupArray;
-
                     m.redraw();
-                    console.log(store.userGroups)
+                   
                 }
 
             })
--- src/views/Groups/Groups.js
+++ src/views/Groups/Groups.js
@@ -31,7 +31,7 @@ module.exports = {
                 <div class='wrapper groupsWrapper'>
                     {
                         store.userGroups.map((group, key) => {
-                            console.log(group)
+
                             return <Group
                                 title={group.title}
                                 description={group.description}
