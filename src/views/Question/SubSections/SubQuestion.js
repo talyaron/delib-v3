9f9f5394f9883177cfe6b941db70ba25352058e4
Tal Yaron <tal.yaron@gmail.com>
2019-02-19 11:55:43 +0200

    cont

--- src/views/Question/SubSections/SubQuestion.js
+++ src/views/Question/SubSections/SubQuestion.js
@@ -10,11 +10,15 @@ module.exports = {
     oninit: vnode => {
         vnode.state = { showAnswers: false }
     },
+    oncreate: vnode => {
+        //hide sub answers
+        vnode.dom.children[1].style.height = '0px';
+    },
     view: (vnode) => {
         let showAnswers = vnode.state.showAnswers
         return (
             <div>
-                <div class='card subQuestionCard' onclick={() => toggleSubQuestion(vnode)}>
+                <div class='card subQuestionCard' onclick={() => toggleSubQuestion(vnode, 120)}>
                     <div class='subQuestionCardCotent'>
                         <div class='subQuestionCardDesc'>{vnode.attrs.description}</div>
                         <div class='subQuestionCardVote optionVote'>
@@ -25,29 +29,24 @@ module.exports = {
                     <div class='subQuestionCardAuthor'>{vnode.attrs.author}</div>
                     <div class='subQuestionCardTalk'>שיחות</div>
                 </div>
-                <div class={showAnswers ? 'subAnswersWrapper' : 'subAnswersWrapper'}>
+                <div class={showAnswers ? 'subAnswersWrapper showAnswers' : 'subAnswersWrapper hideAnswers'}>
                     <SubAnswer text='תשובה כל שהיא' author='יציק שמולי' time='12:34' />
-                    <SubAnswer text='תשובה כל שהיא' author='יציק שמולי' time='12:34' />
-                    <SubAnswer text='תשובה כל שהיא' author='יציק שמולי' time='12:34' />
-                    <SubAnswer text='תשובה כל שהיא' author='יציק שמולי' time='12:34' />
-                    <SubAnswer text='תשובה כל שהיא' author='יציק שמולי' time='12:34' />
-                    <SubAnswer text='תשובה כל שהיא' author='יציק שמולי' time='12:34' />
-                    <SubAnswer text='תשובה כל שהיא' author='יציק שמולי' time='12:34' />
-                    <SubAnswer text='תשובה כל שהיא' author='יציק שמולי' time='12:34' />
-                    <SubAnswer text='תשובה כל שהיא' author='יציק שמולי' time='12:34' />
-                    <SubAnswer text='תשובה כל שהיא' author='יציק שמולי' time='12:34' />
-                    <SubAnswer text='תשובה כל שהיא' author='יציק שמולי' time='12:34' />
+                    <form onsubmit='addAnswer(vnode)' class='addInputForm'>
+                        <textarea placeholder='כתבו את תשובתכם כאן'>
+
+                        </textarea>
+                    </form>
                 </div>
             </div>
         )
     }
 }
 
-function toggleSubQuestion(vnode) {
+function toggleSubQuestion(vnode, height) {
     vnode.state.showAnswers = !vnode.state.showAnswers;
-    console.log(vnode.dom.children[1].clientHeight);
+
     if (vnode.state.showAnswers) {
-        vnode.dom.children[1].style.height = '120px';
+        vnode.dom.children[1].style.height = height + '0px';
     } else {
         vnode.dom.children[1].style.height = '0px';
     }
