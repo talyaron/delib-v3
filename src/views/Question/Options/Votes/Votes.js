28d56d4112c5279c0e0ff268638875fe495fa455
Tal Yaron <tal.yaron@gmail.com>
2019-07-10 09:30:33 +0300

    voting css1

--- src/views/Question/Options/Votes/Votes.js
+++ src/views/Question/Options/Votes/Votes.js
@@ -1,9 +1,44 @@
-import m from 'mithril';
+import m from "mithril";
+import "./Votes.css";
 
 module.exports = {
-    view: vnode => {
-        return (
-            <div>Votes work!</div>
-        )
-    }
+  oninit: vnode => {
+    vnode.state = {
+      options: [
+        { votes: 34, name: "pro" },
+        { votes: 0, name: "abst" },
+        { votes: 31, name: "con" }
+      ]
+    };
+  },
+  view: vnode => {
+    return (
+      <div class="voteWrapper">
+        {vnode.state.options.map((option, index) => {
+           let optionHeight = `${100*option.votes/getMaximumVotes(vnode)}%`;
+           console.log(option.name, optionHeight)
+          return (
+            <div class="voteOption" key={index} style={`height:${optionHeight}`}>
+              <div class='voteColumn' ></div>
+              <div class='voteButton'>voteBtn</div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+};
+
+function getMaximumVotes(vnode){
+    //get maximum votes
+    let votesArray = [];
+    vnode.state.options.forEach(element => {
+        if(typeof element.votes === 'number'){
+        votesArray.push(element.votes);
+        } else {
+            console.error(element.votes, 'is not a number');
+        }
+    });    
+    
+     return Math.max(...votesArray);
 }
